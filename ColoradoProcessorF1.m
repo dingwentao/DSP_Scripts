57955181ca299408d5f5b90a3e8f6f170eb0a0c2
dingwentao <ustc.dingwentao@gmail.com>
2019-04-27 21:03:59 -0500

    add Chunkwise processing code for CO data

---
+++ ColoradoProcessorF1.m
@@ -0,0 +1,56 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SNOW DATA CHUNK PROCESSOR
+% This script is to process each slow-time chunk of snow data
+% Author: Shashank Wattal
+% Version: 1.0
+% Last updated: 04-25-2019
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [data_incoh_f2] = ColoradoProcessorF1(data_t, p)
+    
+    [r0, c0] = size(data_t); 
+   
+    % Convert to frequency domain
+    win0 = hanning(r0);
+    for ii=1:c0
+           data_f(:,ii) = fftshift(fft(data_t(:,ii).*win0, [], 1), 1);
+
+    end
+    
+    % Coherent noise removal and averaging
+    coh_noise = mean(data_f,2);
+    data_f    = data_f - 1.*coh_noise;
+    if p.coh_avg_size==0
+        data_coh_f = data_f;
+    else    
+        for ii=1:c0                    
+            if (ii > p.coh_avg_size) && (ii < c0 - p.coh_avg_size)
+                data_coh_f(:,ii) = mean(data_f(:,ii-p.coh_avg_size:ii+p.coh_avg_size),2);
+            else
+                data_coh_f(:,ii) = data_f(:,ii);
+            end
+        end    
+    end
+
+    data_coh_fN = 20*log10(abs(data_coh_f));
+    data_incoh_f1 = data_coh_fN;
+    % Incoherent averaging
+    if p.incoh_avg_size==0
+        data_incoh_f1 = data_coh_fN;
+    else    
+        for ii=1:c0
+            if (ii > p.incoh_avg_size) && (ii < c0 - p.incoh_avg_size)
+                data_incoh_f1(:,ii) = mean(data_coh_fN(:,ii-p.incoh_avg_size:ii+p.incoh_avg_size),2);
+            else
+                data_incoh_f1(:,ii) = data_coh_fN(:,ii);
+            end
+        end    
+    end
+
+    % Median filter
+    data_incoh_f2 = data_incoh_f1;
+    data_incoh_f2 = medfilt2(data_incoh_f1,[4,4]);
+    data_incoh_f2 = data_incoh_f2 - max(data_incoh_f2);
+end
+
+
---
+++ Colorado_chunk_processor.m
@@ -0,0 +1,109 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PARALLELIZABLE SNOW DATA PROCESSOR
+% This script is to process snow radar data block-wise in slow time
+% Author: Shashank Wattal
+% Version: 1.0
+% Last updated: 04-25-2019
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% data_dir    -   full directory path where the data's stored
+% save_dir    -   full directory path where echograms will be saved     
+% code_dir    -   full directory path where code is stored
+% file0       -   full .mat filename to be processed
+% chunk_size  -   no. of slow time columns in each chunk
+
+% Run single files only
+% In most exception cases, the entire set is processes as a single chunk
+% Params are hard-coded for Grand Junction 2019
+% Calls ColoradoProcessorF1.m to process each chunk
+
+function [] = Colorado_chunk_processor(data_dir, save_dir, code_dir, file0, chunk_size)
+% data_dir =  'K:\Colorado_1';  % full path to data directory
+% save_dir =  data_dir;       % full path to save echograms
+% code_dir =  'J:\RSC\Colorado processing\'; % full path to script/functions directory
+% file0    =  '20190323_145711_MicrowaveRadar2019_CO_0000';
+% numFiles =  1; 
+% chunk_size  =  64;           % no. of slow time columns in a chunk
+
+path(path,code_dir)
+if ~strcmp(data_dir(end), '/') 
+    data_dir = [data_dir '/'];
+end
+if ~strcmp(save_dir(end), '/') 
+    save_dir = [save_dir '/'];
+end
+if ~strcmp(file0(end-3:end), '.mat') 
+    file0 = [file0 '.mat'];
+end
+
+% parameters 
+params.hN = 8 ;                            % num of Pulses integrated over
+params.adc_sampFreq = 1200*10^6;
+params.dec = 4*4;                        %Decimation
+params.coh_avg_size = 4;           % coherent averaging
+params.incoh_avg_size = 4;
+params.NumPoints = 60000;
+params.dac_sampFreq = 2400e6;
+params.pulse_width = params.NumPoints/params.dac_sampFreq;
+params.chirp_rate = 4e9/params.pulse_width;
+params.gps_path = [data_dir file0(1:15) '_ARENA__CTU-CTU-gps.txt'];
+params.Fs = 1200e6/4/4;
+
+if chunk_size <= params.coh_avg_size*params.incoh_avg_size
+    error('Expected chunk size larger than product of coherent average size and incoherent average size!'); 
+end
+    
+load([data_dir file0]);
+load([data_dir file0(1:end-4) '_counters.mat']);
+chunk_size = floor(chunk_size);
+r = size(Results.Chirps, 1);
+c = size(Results.Chirps, 2);
+if (c < chunk_size || chunk_size <= 0)
+    chunk_size = c; 
+end
+
+%%
+for ii = 1 : ceil(c/chunk_size)
+    % indices to subset in slow time
+    first(ii) = (ii-1)*chunk_size+1;
+    last(ii) = ii*chunk_size; 
+    if last(ii)>c
+        last(ii)=c; 
+    end        
+    % pad zeros
+    if length(num2str(ii))==1        ii_str(ii, :)=['00000' num2str(ii)];
+    elseif length(num2str(ii))==2    ii_str(ii, :)=['0000' num2str(ii)];
+    elseif length(num2str(ii))==3    ii_str(ii, :)=['000' num2str(ii)];
+    elseif length(num2str(ii))==4    ii_str(ii, :)=['00' num2str(ii)];
+    elseif length(num2str(ii))==5    ii_str(ii, :)=['0' num2str(ii)];    
+    elseif length(num2str(ii))>6     error('Expected number of chunks < 999,999');     
+    end
+    %     save_path(ii, :) = [save_dir file0(1:end-4) '_chunk' ii_str(ii, :)];
+%     figName(ii, :) = [file0(1:15) '_' file0(39:42) '_' ii_str(ii, :)];
+    D = 2*params.coh_avg_size; 
+    if first(ii)>D && last(ii)<c-D
+        temp = ColoradoProcessorF1(Results.Chirps(:, first(ii)-D:last(ii)+D), params);
+        [echogram(:, first(ii):last(ii))] = temp(:,1+D:end-D);
+    else
+        [echogram(:, first(ii):last(ii))] = ColoradoProcessorF1(Results.Chirps(:, first(ii):last(ii)), params);
+    end
+end
+
+% range axis
+f = params.Fs/2 * linspace(-1,1,r);
+f = f+148e6;
+range1 = f .* 180e-6/8e9 * 3e8/sqrt(1)/2;
+
+% save full echogram
+save_path_fig = [save_dir file0(1:end-4) '.fig']; % '_chunk' ii_str(ii, :)];
+save_path_jpg = [save_dir file0(1:end-4) '.jpg']; % '_chunk' ii_str(ii, :)];
+f1 = figure('visible', 'off');
+imagesc([],range1,echogram)
+colormap(1-gray)
+title([ file0(1:8) '-' file0(10:15) '-' file0(39:42) ])
+ylabel('Range (m) [\epsilon_r=1]')
+xlabel('Along-track index')
+saveas(f1, save_path_fig)
+saveas(f1, save_path_jpg)
+
+end
